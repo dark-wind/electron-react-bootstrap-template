e98cdcb09a194bd135a284f98f0d922db942bf12
dark-wind <shadow@dumii.cn>
2019-05-23 17:38:10 +0800

    + 加入模板

---
+++ src/components/Navbars/AdminNavbarLinks.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { NavItem, Nav, NavDropdown, MenuItem } from "react-bootstrap";
+
+class AdminNavbarLinks extends Component {
+  render() {
+    return (
+      <div>
+        <Nav pullRight>
+            <NavDropdown
+              eventKey={2}
+              title="Setting"
+              id="basic-nav-dropdown-right"
+            >
+              <MenuItem eventKey={2.1}>Action</MenuItem>
+              <MenuItem eventKey={2.2}>Another action</MenuItem>
+              <MenuItem eventKey={2.3}>Something</MenuItem>
+              <MenuItem eventKey={2.4}>Another action</MenuItem>
+              <MenuItem eventKey={2.5}>Something</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={2.5}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+      </div>
+    );
+  }
+}
+
+export default AdminNavbarLinks;
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import Test from "./views/Test";
+
+const dashboardRoutes = [
+  {
+    path: "/test",
+    name: "test",
+    icon: "pe-7s-rocket",
+    component: Test,
+    layout: "/admin"
+  }
+];
+
+export default dashboardRoutes;
