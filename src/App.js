87b1dc6725da5d8979099d6a533213a0ed10b7df
dark-wind <shadow@dumii.cn>
2019-05-23 16:41:35 +0800

    [-] 移除多余的依赖

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Nav from 'react-bootstrap/Nav';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
-      简洁的主页有利于加快启动速度homepage/index.html
+      简洁的主页有利于加快启动速度
       <hr></hr>
     </div>
   );
