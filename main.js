0bf33b31bfc5b60c0cd8142bcc3d45f15685ae87
dark-wind <shadow@dumii.cn>
2019-05-27 11:41:07 +0800

    [+] 给 linux 加logo

--- main.js
+++ main.js
@@ -8,10 +8,9 @@ const pkg = require('./package.json')
 let win
 
 function createWindow() {
-  // 创建浏览器窗口。
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
+  const windowOptions = {
+    width: 1080,
+    height: 840,
     autoHideMenuBar: true,
     fullscreenable: false,
     webPreferences: {
@@ -21,7 +20,13 @@ function createWindow() {
       webSecurity: false,
       preload: path.join(__dirname, './public/renderer.js') // 但预加载的 js 文件内仍可以使用 Nodejs 的 API
     }
-  })
+  }
+  if (process.platform === 'linux') {
+    windowOptions.icon = path.join(__dirname,'./public/ghost.png')
+  }
+
+  // 创建浏览器窗口。
+  win = new BrowserWindow(windowOptions)
 
   // 加载index.html文件
   if (pkg.devMode)
@@ -35,7 +40,7 @@ function createWindow() {
 
 
   // 打开开发者工具
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
 
   // 当 window 被关闭，这个事件会被触发。
   win.on('closed', () => {
